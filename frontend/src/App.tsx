75c6f571ef7359a68c3be1c7df453b753199d946
Alisson Oliveira <alisson.dev.py@gmail.com>
2025-10-16 20:27:36 -0300

    feat: página not found

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import Footer from './components/Footer';
 import LandingPage from './pages/LandingPage';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 const MainLayout = () => {
   return (
@@ -18,7 +19,6 @@ const MainLayout = () => {
   );
 };
 
-
 function App() {
   return (
     <Routes>
@@ -26,6 +26,8 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+
+        <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
   );
